76589826215230904cddd9ad6d731b5e1139e9fc
Sacha Wagener <sacha.wagener@gmail.com>
2026-03-26 21:24:54 +0200

    Theme updates

--- docroot/themes/custom/themekit/js/src/search-header/App.jsx
+++ docroot/themes/custom/themekit/js/src/search-header/App.jsx
@@ -6,31 +6,44 @@ class App extends React.Component {
     this.state = {
       keyword: '',
     };
+    this.inputRef = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.showSearch = this.showSearch.bind(this);
   }
 
-  handleSubmit = (event) => {
+  handleSubmit(event) {
     event.preventDefault();
     window.location.href = '/search?key=' + this.state.keyword;
-  };
+  }
 
-  handleChange = (event) => {
+  handleChange(event) {
     this.setState({ keyword: event.target.value });
-  };
+  }
 
-  showSearch = (event) => {
-    event.target.parentNode.classList.toggle('active');
-  };
+  showSearch(event) {
+    const parent = event.target.parentNode;
+    parent.classList.toggle('active');
+    if (parent.classList.contains('active')) {
+      this.inputRef.current.focus();
+    }
+  }
 
   render() {
     return (
-      <div className="search-header">
-        <form className="search-header--form" onSubmit={ this.handleSubmit }>
-          <input type="text" value={ this.state.keyword } onChange={ this.handleChange }/>
+      <div className="search-header"> 
+        <form className="search-header--form" onSubmit={this.handleSubmit}> 
+          <input
+            ref={this.inputRef}
+            type="text"
+            value={this.state.keyword}
+            onChange={this.handleChange}
+          />
         </form>
 
-        <button onClick={ this.showSearch } className="search-header--search-trigger">Search</button>
+        <button onClick={this.showSearch} className="search-header--search-trigger">Search</button>
       </div>
-    )
+    );
   }
 }
 
